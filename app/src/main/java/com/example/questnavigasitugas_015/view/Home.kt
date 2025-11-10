05a4b8f7d6a7d15178451c3ee871b57cf1d60724
rcslbaa <ifasmarani3125@gmail.com>
2025-11-10 19:57:59 +0700

    membuat composable Home

---
+++ app/src/main/java/com/example/questnavigasitugas_015/view/Home.kt
@@ -0,0 +1,23 @@
+package com.example.questnavigasiui_015.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_015.R
+
+@Composable
+fun Home(
+    OnMasukBtnClick: () -> Unit
+) {
